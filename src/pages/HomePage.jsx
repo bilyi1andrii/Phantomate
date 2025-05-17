30c9c44a59aa0259a11d8d0927d4e890b62993e1
Your Name <you@example.com>
2025-05-18 00:08:33 +0300

    removed ghost gif from chat

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -150,7 +150,7 @@ export default function PhantomatePage() {
                             </AnimatePresence>
                         )}
 
-                        {(
+                        {!isChatMode && (
                             <button className="ghost-button" onClick={toggleChatMode}>
                                 <img
                                     src={GhostImage}
